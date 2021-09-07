ef0edfe8edd09ab50f57c821e61adcfcb5a55ee3
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-09-07 20:16:05 +0100

    Added spring project with file upload support

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/IReceptorChainClientApplication.java
@@ -0,0 +1,14 @@
+package com.ireceptorplus.ireceptorchainclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class IReceptorChainClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IReceptorChainClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ireceptorplus/ireceptorchainclient/IReceptorChainClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ireceptorplus.ireceptorchainclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IReceptorChainClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
