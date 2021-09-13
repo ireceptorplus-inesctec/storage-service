d318f41133be358bf212fc30a09f8c0f72790969
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-09-14 00:15:29 +0100

    fixed compile errors

--- src/main/java/com/ireceptorplus/ireceptorchainclient/DataTransformationRunning/NextFlowScriptRunner.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/DataTransformationRunning/NextFlowScriptRunner.java
@@ -1,5 +1,23 @@
 package com.ireceptorplus.ireceptorchainclient.DataTransformationRunning;
 
+import java.util.ArrayList;
+
 public class NextFlowScriptRunner extends DataTransformationRunner
 {
+    public NextFlowScriptRunner(ArrayList<Dataset> inputs, Script script)
+    {
+        super(inputs, script);
+    }
+
+    @Override
+    void run()
+    {
+
+    }
+
+    @Override
+    boolean verifyIfOutputsMatch(Dataset dataset)
+    {
+        return false;
+    }
 }
