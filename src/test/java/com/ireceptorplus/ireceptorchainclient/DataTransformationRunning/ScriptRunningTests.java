bb1d7364a5fba0bd59e53db6cfcbfd86f2cbfc27
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-09-19 17:50:25 +0100

    added class and file to test script running with next flow

---
+++ src/test/java/com/ireceptorplus/ireceptorchainclient/DataTransformationRunning/ScriptRunningTests.java
@@ -0,0 +1,15 @@
+package com.ireceptorplus.ireceptorchainclient.DataTransformationRunning;
+
+import org.junit.jupiter.api.Test;
+
+public class ScriptRunningTests
+{
+    public class NextFlow
+    {
+        @Test
+        void runBasicPipeline()
+        {
+
+        }
+    }
+}
