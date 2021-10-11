59f0e1e08cf9b8bc9188f88ab5b5f267faa4b62b
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-11 17:13:51 +0100

    added getters and setters to Dataset class

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Dataset.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Dataset.java
@@ -15,4 +15,34 @@ public class Dataset
 
     @ManyToOne
     private ProcessingStep processingStep;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public void setUrl(String url)
+    {
+        this.url = url;
+    }
+
+    public ProcessingStep getProcessingStep()
+    {
+        return processingStep;
+    }
+
+    public void setProcessingStep(ProcessingStep processingStep)
+    {
+        this.processingStep = processingStep;
+    }
 }
