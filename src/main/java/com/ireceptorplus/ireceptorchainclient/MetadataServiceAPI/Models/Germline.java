6bfad2c6a1c5683e6a088b05f09d3868b7c7710e
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-11 18:09:38 +0100

    renamed GermlineDatabase.java to Germline and added string representing url from which the germline file can be fetched

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Germline.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Germline.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 @Entity
 @Table(name = "germline_database")
-public class GermlineDatabase
+public class Germline
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String url;
 
 /*
     @JsonIgnore
@@ -21,14 +24,27 @@ public class GermlineDatabase
 */
 
 
-    public GermlineDatabase() {}
+    public Germline()
+    {
+    }
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
         this.id = id;
     }
 
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public void setUrl(String url)
+    {
+        this.url = url;
+    }
 }
