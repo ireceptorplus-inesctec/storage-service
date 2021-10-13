d6ddb5f8efacfae4f50e01745d623209de1388bc
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-13 15:34:14 +0100

    added create, read all and read by id endpoints on GermlineController

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/GermlineController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/GermlineController.java
@@ -1,11 +1,17 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.GermlineDTO;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Germline;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.GermlineService;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.ToolService;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.GermlineService;
+import com.sun.istack.NotNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("germline")
@@ -24,5 +30,25 @@ public class GermlineController
         this.germlineService = germlineService;
     }
 
+    @GetMapping
+    public List<Germline> getGermlines()
+    {
+        return germlineService.readAll();
+    }
 
+
+    @GetMapping("/{id}")
+    public Optional<Germline> getGermline(@PathVariable @NotNull Long id) {
+        return germlineService.readById(id);
+    }
+
+    @PostMapping
+    public GermlineDTO createGermline(@RequestBody @Valid GermlineDTO germlineDTO)
+    {
+        Germline germline = modelMapper.map(germlineDTO, Germline.class);
+        Germline createdGermline = germlineService.create(germline);
+        GermlineDTO createdGermlineDTO = modelMapper.map(createdGermline, GermlineDTO.class);
+
+        return createdGermlineDTO;
+    }
 }
