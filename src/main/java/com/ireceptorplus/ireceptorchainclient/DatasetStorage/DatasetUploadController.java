1ce8df8b4c5b597461ae4fb9df65ef93d59c68a8
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-13 16:24:04 +0100

    fixed duplicated endpoint name on file controller and metadata controller

--- src/main/java/com/ireceptorplus/ireceptorchainclient/DatasetStorage/DatasetUploadController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/DatasetStorage/DatasetUploadController.java
@@ -26,7 +26,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class DatasetUploadController {
 
     private final StorageService storageService;
-    protected static final String contentName = "dataset";
+    protected static final String contentName = "dataset_file";
 
     @Autowired
     public DatasetUploadController(StorageService storageService) {
