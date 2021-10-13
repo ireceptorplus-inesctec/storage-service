5cc14c78395e7472971fa1f231058d451b8401ff
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-13 15:18:59 +0100

    added missing @Service annotation to GermlineService

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/GermlineController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/GermlineController.java
@@ -1,5 +1,28 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.GermlineService;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.ToolService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("germline")
 public class GermlineController
 {
+
+    @Autowired
+    private final GermlineService germlineService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    public GermlineController(GermlineService germlineService)
+    {
+        this.germlineService = germlineService;
+    }
+
+
 }
