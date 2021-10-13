d65de0e8d21fa7731929e87f8cad0cdb521f3eb9
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-13 16:26:24 +0100

    fixed wrong names from copy-paste on DatasetController

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/DatasetController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/DatasetController.java
@@ -1,17 +1,18 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.ToolDTO;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Dataset;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Tool;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Dataset;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.DatasetService;
+import com.sun.istack.NotNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("dataset")
@@ -28,12 +29,25 @@ public class DatasetController
         this.datasetService = datasetService;
     }
 
-    public DatasetDTO create(@RequestBody @Valid DatasetDTO datasetDTO)
+    @GetMapping
+    public List<Dataset> getDatasets()
     {
-        Dataset dataset = modelMapper.map(datasetDTO, Dataset.class);
-        Dataset createdTool = datasetService.create(dataset);
-        DatasetDTO createdToolDTO = modelMapper.map(createdTool, DatasetDTO.class);
+        return datasetService.readAll();
+    }
 
-        return createdToolDTO;
+
+    @GetMapping("/{id}")
+    public Optional<Dataset> getDataset(@PathVariable @NotNull Long id) {
+        return datasetService.readById(id);
+    }
+
+    @PostMapping
+    public DatasetDTO createDataset(@RequestBody @Valid DatasetDTO toolDTO)
+    {
+        Dataset tool = modelMapper.map(toolDTO, Dataset.class);
+        Dataset createdDataset = datasetService.create(tool);
+        DatasetDTO createdDatasetDTO = modelMapper.map(createdDataset, DatasetDTO.class);
+
+        return createdDatasetDTO;
     }
 }
