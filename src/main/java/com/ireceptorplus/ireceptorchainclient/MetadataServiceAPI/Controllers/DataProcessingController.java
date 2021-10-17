0f723536921617b1c42589e8f43b98540550b97a
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-17 17:30:20 +0100

    added endpoint on DataProcessingController not working yet

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/DataProcessingController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/DataProcessingController.java
@@ -1,27 +1,46 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DataProcessingDTO;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Mappers.DataProcessingMapper;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.DataProcessing;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.DataProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
-@RequestMapping(path = "api/v1/dataProcessing")
+@RestController
+@RequestMapping("data_processing")
 public class DataProcessingController
 {
+    @Autowired
     private final DataProcessingService dataProcessingService;
 
     @Autowired
-    public DataProcessingController(DataProcessingService dataProcessingService)
+    private final DataProcessingMapper dataProcessingMapper;
+
+    public DataProcessingController(DataProcessingService dataProcessingService, DataProcessingMapper dataProcessingMapper)
     {
         this.dataProcessingService = dataProcessingService;
+        this.dataProcessingMapper = dataProcessingMapper;
     }
+
 
     @GetMapping
     public List<DataProcessing> getDataProcessing()
     {
         return dataProcessingService.getDataProcessings();
     }
+
+    @PostMapping
+    public DataProcessingDTO create(@RequestBody @Valid DataProcessingDTO dataProcessingDTO)
+    {
+        DataProcessing dataProcessing = dataProcessingMapper.dataProcessingDTOtoDataProcessing(dataProcessingDTO);
+        DataProcessing createdDataProcessing = dataProcessingService.create(dataProcessing);
+        DataProcessingDTO createdDataProcessingDTO = dataProcessingMapper.dataProcessingtoDataProcessingDTO(createdDataProcessing);
+
+        return createdDataProcessingDTO;
+    }
+
 }
