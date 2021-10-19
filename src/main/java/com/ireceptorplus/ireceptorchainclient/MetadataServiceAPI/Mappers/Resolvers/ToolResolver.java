722234e52ad218d77b642d618f604522b8fbfc40
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-19 20:09:39 +0100

    added ToolResolver

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Mappers/Resolvers/ToolResolver.java
@@ -0,0 +1,18 @@
+package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Mappers.Resolvers;
+
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Tool;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ToolResolver extends ResolverById<Tool>
+{
+    public ToolResolver(JpaRepository<Tool, Long> repository)
+    {
+        super(repository);
+    }
+
+    @Override
+    public Tool getNewEntity()
+    {
+        return new Tool();
+    }
+}
