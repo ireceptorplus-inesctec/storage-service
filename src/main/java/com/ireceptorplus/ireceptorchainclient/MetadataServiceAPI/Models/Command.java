64c1f4a35614f4d3501f41509990d6c5574f52be
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-10-19 18:48:28 +0100

    added missing default constructor and Getters and Setters on class Command

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Command.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Command.java
@@ -22,4 +22,48 @@ public class Command
      */
     @Column
     private String commandString;
+
+    public Command()
+    {
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public ProcessingStep getProcessingStep()
+    {
+        return processingStep;
+    }
+
+    public void setProcessingStep(ProcessingStep processingStep)
+    {
+        this.processingStep = processingStep;
+    }
+
+    public Tool getTool()
+    {
+        return tool;
+    }
+
+    public void setTool(Tool tool)
+    {
+        this.tool = tool;
+    }
+
+    public String getCommandString()
+    {
+        return commandString;
+    }
+
+    public void setCommandString(String commandString)
+    {
+        this.commandString = commandString;
+    }
 }
