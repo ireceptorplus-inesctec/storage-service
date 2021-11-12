e365710b5f03ae5b682890c8950f0725240dd993
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-11-12 03:39:54 +0000

    added support on DatasetController create dataset endpoint to receive metadata and file on the same request

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/DatasetController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/DatasetController.java
@@ -1,33 +1,40 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ireceptorplus.ireceptorchainclient.DatasetStorage.storage.StorageService;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Dataset;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Dataset;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.DatasetService;
 import com.sun.istack.NotNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/dataset")
 public class DatasetController
 {
     @Autowired
-    private final DatasetService datasetService;
+    private DatasetService datasetService;
 
     @Autowired
     private ModelMapper modelMapper;
 
-    public DatasetController(DatasetService datasetService)
-    {
-        this.datasetService = datasetService;
-    }
+    @Autowired
+    private StorageService storageService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping
     public List<Dataset> getDatasets()
@@ -41,11 +48,25 @@ public class DatasetController
         return datasetService.readById(id);
     }
 
-    @PostMapping
-    public DatasetDTO createDataset(@RequestBody @Valid DatasetDTO toolDTO)
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public DatasetDTO createDataset(@RequestParam("dataset") String datasetStr,
+                                    @RequestParam("file") MultipartFile file)
     {
-        Dataset tool = modelMapper.map(toolDTO, Dataset.class);
-        Dataset createdDataset = datasetService.create(tool);
+        DatasetDTO datasetDTO = null;
+        try
+        {
+            datasetDTO = objectMapper.readValue(datasetStr, DatasetDTO.class);
+        } catch (JsonProcessingException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        storageService.store(file);
+
+        Dataset dataset = modelMapper.map(datasetDTO, Dataset.class);
+        dataset.setCreatedDate(new Date());
+
+        Dataset createdDataset = datasetService.create(dataset);
         DatasetDTO createdDatasetDTO = modelMapper.map(createdDataset, DatasetDTO.class);
 
         return createdDatasetDTO;
