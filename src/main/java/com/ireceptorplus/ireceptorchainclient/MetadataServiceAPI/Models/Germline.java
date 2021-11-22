199c0bfbd22741d0fbd9418f527e2f99dba38a14
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-11-22 03:35:08 +0000

    added missing fields to Germline and GermlineDTO

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Germline.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Germline.java
@@ -3,7 +3,9 @@ package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 public class Germline
@@ -12,16 +14,26 @@ public class Germline
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * This identifies the .fasta file in the file system and, therefore, defines the file name.
+     */
     @Column
-    private String url;
+    private UUID uuid;
 
-/*
-    @JsonIgnore
-    @Column(name = "data_processings")
-    @OneToMany(mappedBy = "germlineDatabase", cascade = CascadeType.ALL)
-    private List<DataProcessing> dataProcessings;
-*/
+    /**
+     * A String representing the original file name in the computer of the uploader.
+     */
+    @Column
+    private String originalFileName;
 
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private Date creationDate;
 
     public Germline()
     {
@@ -37,13 +49,53 @@ public class Germline
         this.id = id;
     }
 
-    public String getUrl()
+    public UUID getUuid()
     {
-        return url;
+        return uuid;
     }
 
-    public void setUrl(String url)
+    public void setUuid(UUID uuid)
     {
-        this.url = url;
+        this.uuid = uuid;
+    }
+
+    public String getOriginalFileName()
+    {
+        return originalFileName;
+    }
+
+    public void setOriginalFileName(String originalFileName)
+    {
+        this.originalFileName = originalFileName;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Date getCreationDate()
+    {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate)
+    {
+        this.creationDate = creationDate;
     }
 }
