ff27e3db19207884ec998ec40d229e90c0edb3c7
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2021-11-24 03:31:11 +0000

    added support to receive sequence file on GermlineController

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/GermlineController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/GermlineController.java
@@ -1,5 +1,10 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ireceptorplus.ireceptorchainclient.FileStorage.DatasetStorageService;
+import com.ireceptorplus.ireceptorchainclient.FileStorage.GermlineStorageService;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.GermlineDTO;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Germline;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.GermlineService;
@@ -8,10 +13,13 @@ import com.sun.istack.NotNull;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/germline")
@@ -23,6 +31,12 @@ public class GermlineController
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private GermlineStorageService storageService;
 
 
     public GermlineController(GermlineService germlineService)
@@ -43,9 +57,28 @@ public class GermlineController
     }
 
     @PostMapping
-    public GermlineDTO createGermline(@RequestBody @Valid GermlineDTO germlineDTO)
+    public GermlineDTO createGermline(@RequestParam("dataset") String germlineStr,
+                                      @RequestParam("file") MultipartFile file)
     {
+        GermlineDTO germlineDTO;
+        try
+        {
+            germlineDTO = objectMapper.readValue(germlineStr, GermlineDTO.class);
+        } catch (JsonProcessingException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        String originalFileName = file.getOriginalFilename();
+        UUID uuid = UUID.randomUUID();
+        germlineDTO.setUuid(uuid);
+        storageService.store(file, uuid.toString());
+
         Germline germline = modelMapper.map(germlineDTO, Germline.class);
+        germline.setCreationDate(new Date());
+        germline.setOriginalFileName(originalFileName);
+
         Germline createdGermline = germlineService.create(germline);
         GermlineDTO createdGermlineDTO = modelMapper.map(createdGermline, GermlineDTO.class);
 
