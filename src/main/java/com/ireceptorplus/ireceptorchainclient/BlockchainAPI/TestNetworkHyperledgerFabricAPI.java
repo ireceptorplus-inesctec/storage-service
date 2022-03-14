9fcafce9f10521902f71160f00acf4194d4d2f10
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-14 11:19:39 +0000

    created derived class for blockchain API for the test network

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/TestNetworkHyperledgerFabricAPI.java
@@ -0,0 +1,187 @@
+package com.ireceptorplus.ireceptorchainclient.BlockchainAPI;
+
+import org.hyperledger.fabric.gateway.*;
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.User;
+import org.hyperledger.fabric.sdk.exception.CryptoException;
+import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import org.hyperledger.fabric.sdk.security.CryptoSuiteFactory;
+import org.hyperledger.fabric_ca.sdk.EnrollmentRequest;
+import org.hyperledger.fabric_ca.sdk.HFCAClient;
+import org.hyperledger.fabric_ca.sdk.RegistrationRequest;
+import org.hyperledger.fabric_ca.sdk.exception.EnrollmentException;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.PrivateKey;
+import java.security.cert.CertificateException;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.TimeoutException;
+
+public class TestNetworkHyperledgerFabricAPI extends HyperledgerFabricAPI
+{
+
+    String blockchainDirectoryPath = "../ireceptorchain/";
+
+    public static void main(String[] args) throws Exception {
+        TestNetworkHyperledgerFabricAPI api = new TestNetworkHyperledgerFabricAPI();
+        api.enrollAdmin();
+    }
+
+    private String resolveBlockchainCertsDirPath(String relativePath)
+    {
+        return blockchainDirectoryPath + relativePath;
+    }
+
+    private void enrollAdmin() throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvalidArgumentException, CryptoException, CertificateException, org.hyperledger.fabric_ca.sdk.exception.InvalidArgumentException, EnrollmentException
+    {
+        // Create a CA client for interacting with the CA.
+        Properties props = new Properties();
+        props.put("pemFile",
+                resolveBlockchainCertsDirPath("test-network/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"));
+        props.put("allowAllHostNames", "true");
+        HFCAClient caClient = HFCAClient.createNewInstance("https://localhost:7054", props);
+        CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
+        caClient.setCryptoSuite(cryptoSuite);
+
+        // Create a wallet for managing identities
+        Wallet wallet = Wallets.newFileSystemWallet(Paths.get("wallet"));
+
+        // Check to see if we've already enrolled the admin user.
+        if (wallet.get("admin") != null) {
+            System.out.println("An identity for the admin user \"admin\" already exists in the wallet");
+            return;
+        }
+
+        // Enroll the admin user, and import the new identity into the wallet.
+        final EnrollmentRequest enrollmentRequestTLS = new EnrollmentRequest();
+        enrollmentRequestTLS.addHost("localhost");
+        enrollmentRequestTLS.setProfile("tls");
+        Enrollment enrollment = caClient.enroll("admin", "adminpw", enrollmentRequestTLS);
+        Identity user = Identities.newX509Identity("Org1MSP", enrollment);
+        wallet.put("admin", user);
+        System.out.println("Successfully enrolled user \"admin\" and imported it into the wallet");
+    }
+
+    public void registerUser() throws Exception
+    {
+
+        // Create a CA client for interacting with the CA.
+        Properties props = new Properties();
+        props.put("pemFile",
+                resolveBlockchainCertsDirPath("test-network/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"));
+        props.put("allowAllHostNames", "true");
+        HFCAClient caClient = HFCAClient.createNewInstance("https://localhost:7054", props);
+        CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
+        caClient.setCryptoSuite(cryptoSuite);
+
+        // Create a wallet for managing identities
+        Wallet wallet = Wallets.newFileSystemWallet(Paths.get("wallet"));
+
+        // Check to see if we've already enrolled the user.
+        if (wallet.get("appUser") != null) {
+            System.out.println("An identity for the user \"appUser\" already exists in the wallet");
+            return;
+        }
+
+        X509Identity adminIdentity = (X509Identity)wallet.get("admin");
+        if (adminIdentity == null) {
+            System.out.println("\"admin\" needs to be enrolled and added to the wallet first");
+            return;
+        }
+        User admin = new User() {
+
+            @Override
+            public String getName() {
+                return "admin";
+            }
+
+            @Override
+            public Set<String> getRoles() {
+                return null;
+            }
+
+            @Override
+            public String getAccount() {
+                return null;
+            }
+
+            @Override
+            public String getAffiliation() {
+                return "org1.department1";
+            }
+
+            @Override
+            public Enrollment getEnrollment() {
+                return new Enrollment() {
+
+                    @Override
+                    public PrivateKey getKey() {
+                        return adminIdentity.getPrivateKey();
+                    }
+
+                    @Override
+                    public String getCert() {
+                        return Identities.toPemString(adminIdentity.getCertificate());
+                    }
+                };
+            }
+
+            @Override
+            public String getMspId() {
+                return "Org1MSP";
+            }
+
+        };
+
+        // Register the user, enroll the user, and import the new identity into the wallet.
+        RegistrationRequest registrationRequest = new RegistrationRequest("appUser");
+        registrationRequest.setAffiliation("org1.department1");
+        registrationRequest.setEnrollmentID("appUser");
+        String enrollmentSecret = caClient.register(registrationRequest, admin);
+        Enrollment enrollment = caClient.enroll("appUser", enrollmentSecret);
+        Identity user = Identities.newX509Identity("Org1MSP", enrollment);
+        wallet.put("appUser", user);
+        System.out.println("Successfully enrolled user \"appUser\" and imported it into the wallet");
+
+    }
+
+    public void clientApp() throws IOException, ContractException, InterruptedException, TimeoutException
+    {
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a CCP
+        Path networkConfigPath = Paths.get(resolveBlockchainCertsDirPath(""), "test-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+
+        Gateway.Builder builder = Gateway.createBuilder();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+
+        // create a gateway connection
+        try (Gateway gateway = builder.connect()) {
+
+            // get the network and contract
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("fabcar");
+
+            byte[] result;
+
+            result = contract.evaluateTransaction("queryAllCars");
+            System.out.println(new String(result));
+
+            contract.submitTransaction("createCar", "CAR10", "VW", "Polo", "Grey", "Mary");
+
+            result = contract.evaluateTransaction("queryCar", "CAR10");
+            System.out.println(new String(result));
+
+            contract.submitTransaction("changeCarOwner", "CAR10", "Archie");
+
+            result = contract.evaluateTransaction("queryCar", "CAR10");
+            System.out.println(new String(result));
+        }
+    }
+}
