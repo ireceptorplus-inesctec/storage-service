4b223d695525d3cd44fc7bf75407d7de3c3e83ad
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-22 23:54:28 +0000

    extracted method and improved log messages

--- src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/HyperledgerFabricAPI.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/HyperledgerFabricAPI.java
@@ -1,11 +1,14 @@
 package com.ireceptorplus.ireceptorchainclient.BlockchainAPI;
 
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.TraceabilityData.TraceabilityDataAwaitingValidation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.gateway.*;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 public class HyperledgerFabricAPI implements BlockchainAPI
 {
@@ -26,17 +29,14 @@ public class HyperledgerFabricAPI implements BlockchainAPI
     public TraceabilityDataAwaitingValidation getTraceabilityDataAwaitingValidation()
     {
         Gateway.Builder builder = setupHyperledgerFabricGatewayBuilder();
+        Contract contract = setupContract(builder);
 
-        // create a gateway connection
-        try (Gateway gateway = builder.connect()) {
+        byte[] result;
 
-            // get the network and contract
-            Network network = gateway.getNetwork(hyperledgerNetworkDetails.networkName);
-            Contract contract = network.getContract(hyperledgerNetworkDetails.chaincodeId);
-
-            byte[] result;
-
+        try
+        {
             result = contract.evaluateTransaction("getAllAwaitingValidationTraceabilityDataEntries");
+
             System.out.println("Result from blockchain:");
             System.out.println(new String(result));
 
@@ -44,6 +44,7 @@ public class HyperledgerFabricAPI implements BlockchainAPI
 
         } catch (ContractException e)
         {
+            LogFactory.getLog(HyperledgerFabricAPI.class).error("Error fetching data awaiting validation from blockchain");
             e.printStackTrace();
         }
 
@@ -60,6 +61,7 @@ public class HyperledgerFabricAPI implements BlockchainAPI
             wallet = Wallets.newFileSystemWallet(walletPath);
         } catch (IOException e)
         {
+            LogFactory.getLog(HyperledgerFabricAPI.class).error("Error setting up Hyperledger Fabric connection: opening wallet files failed");
             e.printStackTrace();
         }
         // load a CCP
@@ -71,9 +73,20 @@ public class HyperledgerFabricAPI implements BlockchainAPI
             builder.identity(wallet, hyperledgerWalletDetails.userId).networkConfig(networkConfigPath).discovery(true);
         } catch (IOException e)
         {
+            LogFactory.getLog(HyperledgerFabricAPI.class).error("Error setting up Hyperledger Fabric connection: failed creation of builder");
             e.printStackTrace();
         }
         return builder;
+    }
+
+    private Contract setupContract(Gateway.Builder builder)
+    {
+        Gateway gateway = builder.connect();
+        // get the network and contract
+        Network network = gateway.getNetwork(hyperledgerNetworkDetails.networkName);
+        Contract contract = network.getContract(hyperledgerNetworkDetails.chaincodeId);
+
+        return contract;
     }
 
     @Override
