929f6d7592facd32459e78f45d000865a45391d4
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-22 16:20:28 +0000

    optimized imports on HyperledgerFabricAPI

--- src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/HyperledgerFabricAPI.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/HyperledgerFabricAPI.java
@@ -2,26 +2,10 @@ package com.ireceptorplus.ireceptorchainclient.BlockchainAPI;
 
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.TraceabilityData.TraceabilityDataAwaitingValidation;
 import org.hyperledger.fabric.gateway.*;
-import org.hyperledger.fabric.sdk.Enrollment;
-import org.hyperledger.fabric.sdk.User;
-import org.hyperledger.fabric.sdk.exception.CryptoException;
-import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
-import org.hyperledger.fabric.sdk.security.CryptoSuite;
-import org.hyperledger.fabric.sdk.security.CryptoSuiteFactory;
-import org.hyperledger.fabric_ca.sdk.EnrollmentRequest;
-import org.hyperledger.fabric_ca.sdk.HFCAClient;
-import org.hyperledger.fabric_ca.sdk.RegistrationRequest;
-import org.hyperledger.fabric_ca.sdk.exception.EnrollmentException;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.PrivateKey;
-import java.security.cert.CertificateException;
-import java.util.Properties;
-import java.util.Set;
-import java.util.concurrent.TimeoutException;
 
 public class HyperledgerFabricAPI implements BlockchainAPI
 {
