5c72b77073ca10a19f393cd3ecc63f3e4ff96fc4
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-23 00:16:07 +0000

    added exception classes

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/Exceptions/BlockchainAPIException.java
@@ -0,0 +1,9 @@
+package com.ireceptorplus.ireceptorchainclient.BlockchainAPI.Exceptions;
+
+public class BlockchainAPIException extends Exception
+{
+    public BlockchainAPIException(String message)
+    {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/Exceptions/ErrorSettingUpConnection.java
@@ -0,0 +1,9 @@
+package com.ireceptorplus.ireceptorchainclient.BlockchainAPI.Exceptions;
+
+public class ErrorSettingUpConnection extends HyperledgerFabricException
+{
+    public ErrorSettingUpConnection(String message)
+    {
+        super(message);
+    }
+}
