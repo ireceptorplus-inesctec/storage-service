54007772f09406c3019b16f241502771f8fc5540
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-03-28 16:14:34 +0100

    refactored class that represents traceability data to be submitted: removed inheritance from TraceabilityData class and added fields directly on it

--- src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/DataClasses/TraceabilityDataToBeSubmitted.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/DataClasses/TraceabilityDataToBeSubmitted.java
@@ -1,17 +1,46 @@
 package com.ireceptorplus.ireceptorchainclient.BlockchainAPI.DataClasses;
 
-public class TraceabilityDataToBeSubmitted extends TraceabilityData
+import java.util.ArrayList;
+
+public class TraceabilityDataToBeSubmitted
 {
-    public TraceabilityDataToBeSubmitted(String inputDatasetHashValue,
-                                         String outputDatasetHashValue)
+    /**
+     * The hash value of the input dataset used to perform the data transformation.
+     * This is used to validate the integrity of the input dataset, in order to be able to verify the traceability information.
+     */
+    protected final String inputDatasetHashValue;
+
+    /**
+     * The hash value of the output dataset used to perform the data transformation.
+     * This is used to validate the integrity of the input dataset, in order to be able to verify the traceability information.
+     */
+    protected final String outputDatasetHashValue;
+
+    /**
+     * This is an instance of the class ProcessingDetails which contains information regarding the steps taken to perform the data transformation.
+     * These steps are necessary in order to check the veracity of the traceability information.
+     */
+    protected final ProcessingDetails processingDetails;
+
+    /**
+     * The value of this traceability data that will be used to calculate rewards and penalties for the voters.
+     * Optionally, the creator may decide to include an additional reward that will be split among the traceability data validators.
+     * The double representing the reward will be available to be consulted even after the traceability data is registered as validated.
+     */
+    protected Double value;
+
+    public TraceabilityDataToBeSubmitted(String inputDatasetHashValue, String outputDatasetHashValue, ProcessingDetails processingDetails)
     {
-        super(inputDatasetHashValue, outputDatasetHashValue, new NullEntityID());
+        this.inputDatasetHashValue = inputDatasetHashValue;
+        this.outputDatasetHashValue = outputDatasetHashValue;
+        this.processingDetails = processingDetails;
     }
 
-    public TraceabilityDataToBeSubmitted(String inputDatasetHashValue,
-                                         String outputDatasetHashValue,
-                                         Double value)
+    public TraceabilityDataToBeSubmitted(String inputDatasetHashValue, String outputDatasetHashValue, ProcessingDetails processingDetails, Double value)
     {
-        super(inputDatasetHashValue, outputDatasetHashValue, new NullEntityID(), value);
+        this.inputDatasetHashValue = inputDatasetHashValue;
+        this.outputDatasetHashValue = outputDatasetHashValue;
+        this.processingDetails = processingDetails;
+        this.value = value;
     }
 }
