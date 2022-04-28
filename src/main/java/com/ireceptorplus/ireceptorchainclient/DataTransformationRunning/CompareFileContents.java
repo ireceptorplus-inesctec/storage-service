a5955850b66f9980d0c884ad8d6973fae3c7cf40
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-04-28 18:41:59 +0100

    added class to compare file contents

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/DataTransformationRunning/CompareFileContents.java
@@ -0,0 +1,24 @@
+package com.ireceptorplus.ireceptorchainclient.DataTransformationRunning;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+public class CompareFileContents {
+
+    private String file1, file2;
+
+    public CompareFileContents(String file1, String file2)
+    {
+        this.file1 = file1;
+        this.file2 = file2;
+    }
+
+    public boolean compare() throws IOException
+    {
+        File file1_ = new File(file1);
+        File file2_ = new File(file2);
+        return FileUtils.contentEquals(file1_, file2_);
+    }
+}
