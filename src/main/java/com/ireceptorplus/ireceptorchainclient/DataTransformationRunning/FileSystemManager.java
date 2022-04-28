7624ab2fe96e9dddc75b5ff91ea9961aa19f8572
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-04-28 20:28:48 +0100

    added class FileSystemManager to encapsulate logic of managing files: input and output datasets and script

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/DataTransformationRunning/FileSystemManager.java
@@ -0,0 +1,72 @@
+package com.ireceptorplus.ireceptorchainclient.DataTransformationRunning;
+
+import com.ireceptorplus.ireceptorchainclient.BlockchainAPI.DataClasses.ReproducibilityData.DownloadbleFile;
+import com.ireceptorplus.ireceptorchainclient.BlockchainAPI.DataClasses.ReproducibilityData.ReproducibleScript;
+
+public class FileSystemManager
+{
+    public String getInputsRelativePath()
+    {
+        return "./";
+    }
+
+    public String getScriptRelativePath()
+    {
+        return "./";
+    }
+
+    public String getExpectedOutputsRelativePath()
+    {
+        return "./outputs";
+    }
+
+    public String getProcessedOutputsRelativePath()
+    {
+        return "./";
+    }
+
+    public String getInputRelativePath(DownloadbleFile input)
+    {
+        return getInputsRelativePath() + getFileName(input);
+    }
+
+    public String getScriptRelativePath(ReproducibleScript script)
+    {
+        return getScriptRelativePath() + getFileName(script);
+    }
+
+    public String getExpectedOutputRelativePath(DownloadbleFile expectedOutput)
+    {
+        return getExpectedOutputsRelativePath() + getFileName(expectedOutput);
+    }
+
+    public String getProcessedOutputsRelativePath(DownloadbleFile processedOutput)
+    {
+        return getProcessedOutputsRelativePath() + getFileName(processedOutput);
+    }
+
+    private String getInputFileName(DownloadbleFile file)
+    {
+        return getFileName(file);
+    }
+
+    private String getScriptFileName(DownloadbleFile file)
+    {
+        return getFileName(file);
+    }
+
+    private String getExpectedOutputFileName(DownloadbleFile file)
+    {
+        return getFileName(file);
+    }
+
+    private String getProcessedOutputFileName(DownloadbleFile file)
+    {
+        return getFileName(file);
+    }
+
+    private String getFileName(DownloadbleFile file)
+    {
+        return file.getHashValue();
+    }
+}
