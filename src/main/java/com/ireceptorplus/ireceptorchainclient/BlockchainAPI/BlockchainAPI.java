996923d5ffcfb809eb5b9786b933704258bc428c
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-05-02 18:06:46 +0100

    documented class BlockchainAPI

--- src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/BlockchainAPI.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/BlockchainAPI/BlockchainAPI.java
@@ -8,12 +8,34 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/**
+ * This class implements the logic for contacting the blockchain.
+ */
 @Component
 public interface BlockchainAPI
 {
+    /**
+     * Creates a traceability data entry on the blockchain.
+     * @param data An instance of class TraceabilityDataToBeSubmitted that represents the traceability data entry to be created on the blockchain.
+     * @return An instance of class TraceabilityDataReturnType that represents the traceability data entry that has just been created on the blockchain.
+     * @throws BlockchainAPIException  An exception is thrown when there is an error contacting the blockchain or parsing the result. It can be a connection error, blockchain certificate error, user certificate error, permission denied, etc. The exception message and exception class subtype provide detailed information about the error.
+     */
     TraceabilityDataReturnType createTraceabilityDataEntry(TraceabilityDataToBeSubmitted data) throws BlockchainAPIException;
 
+    /**
+     * Fetches all the Traceability data entries on the blockchain that are awaiting validation.
+     * These are the entries that the user can vote for and, eventually, be rewarded if considered correct by the majority of the network.
+     * @return A List of TraceabilityDataReturnType representing the traceability data entries that are currently awaiting validation on the blockchain.
+     * @throws BlockchainAPIException  An exception is thrown when there is an error contacting the blockchain or parsing the result. It can be a connection error, blockchain certificate error, user certificate error, permission denied, etc. The exception message and exception class subtype provide detailed information about the error.
+     */
     List<TraceabilityDataReturnType> getTraceabilityDataAwaitingValidation() throws BlockchainAPIException;
 
+    /**
+     * Submits a vote to a traceability data entry on the blockchain.
+     * @param uuid The uuid of the traceability data entry to vote for.
+     * @param voteType An instance of class VoteType which is an enum type that can be either YES or NO, telling whether the user wants to vote yes or not, respectively, for the traceability data entry.
+     * @return An instance of VoteResultReturnType representing the vote just submitted to the blockchain if it was successfully submitted.
+     * @throws BlockchainAPIException  An exception is thrown when there is an error contacting the blockchain or parsing the result. It can be a connection error, blockchain certificate error, user certificate error, permission denied, etc. The exception message and exception class subtype provide detailed information about the error.
+     */
     VoteResultReturnType submitVote(String uuid, VoteType voteType) throws BlockchainAPIException;
 }
