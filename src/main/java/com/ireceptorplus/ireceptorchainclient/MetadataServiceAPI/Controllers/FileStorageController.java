03ab4a3c11c3899b75d89ff5e05df7dbfddc4c84
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-05-20 01:07:51 +0100

    added FileStorageController to allow retrieving stored files to the front-end

---
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/FileStorageController.java
@@ -0,0 +1,33 @@
+package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
+
+import com.ireceptorplus.ireceptorchainclient.FileStorage.DatasetStorageService;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Dataset;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/file/{uuid}")
+@CrossOrigin(origins = "${app.resourceAllowedOrigins}")
+public class FileStorageController
+{
+
+    @Autowired
+    private DatasetStorageService storageService;
+
+    @GetMapping
+    @Operation(summary = "Returns all Dataset objects stored")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename)
+    {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+}
