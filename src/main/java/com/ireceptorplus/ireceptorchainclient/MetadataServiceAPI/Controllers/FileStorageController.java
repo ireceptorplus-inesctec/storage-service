2d60638ba25efa1175ac6610d2f5f6c318c539fb
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-05-20 11:36:33 +0100

    fixed getting uuid from path on FileStorageController

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/FileStorageController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/FileStorageController.java
@@ -1,17 +1,12 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
 import com.ireceptorplus.ireceptorchainclient.FileStorage.DatasetStorageService;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.DatasetDTO;
-import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Dataset;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/file")
@@ -24,9 +19,9 @@ public class FileStorageController
 
     @GetMapping("/{uuid}")
     @Operation(summary = "Returns all Dataset objects stored")
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename)
+    public ResponseEntity<Resource> serveFile(@PathVariable String uuid)
     {
-        Resource file = storageService.loadAsResource(filename);
+        Resource file = storageService.loadAsResource(uuid);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
