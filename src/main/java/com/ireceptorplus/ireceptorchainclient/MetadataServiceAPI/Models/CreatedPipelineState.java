0bb8cec566184035fb98e7f2cdc597e904963ff9
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-06-02 17:11:47 +0100

    pinned ordinal values of enum CreatedPipelineState to ensure expansibility

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/CreatedPipelineState.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/CreatedPipelineState.java
@@ -2,5 +2,16 @@ package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models;
 
 public enum CreatedPipelineState
 {
-    JUST_CREATED, IN_QUEUE, PROCESSING, FINISHED
+    JUST_CREATED(0), IN_QUEUE(1), PROCESSING(2), FINISHED(3);
+
+    private int value;
+
+    private CreatedPipelineState(int value) {
+        this.value = value;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
 }
