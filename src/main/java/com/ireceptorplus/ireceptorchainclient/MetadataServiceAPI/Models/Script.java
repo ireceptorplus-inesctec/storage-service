b90a32388051daf37b3c5df481f723865b40f997
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-06-04 02:55:17 +0100

    added missing script type to Script model

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Script.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Models/Script.java
@@ -1,11 +1,15 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models;
 
+import com.ireceptorplus.ireceptorchainclient.BlockchainAPI.DataClasses.ReproducibilityData.ReproducibleScript;
+
 import javax.persistence.*;
 
 @Entity
 @Table
 public class Script
 {
+    public enum ScriptType {NEXTFLOW, BASH}
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,10 +19,21 @@ public class Script
     private CreatedPipeline createdPipeline;
 
     /**
+     * An enum type that describes the type of script. Can be either NEXTFLOW or BASH.
+     */
+    private ReproducibleScript.ScriptType scriptType;
+
+    /**
      * A string representing the uuid of the script that can be used to index it in the filesystem.
      */
     @Column
     private String uuid;
+
+    /**
+     * A string representing the url of the script that can be used to fetch it from another peer.
+     */
+    @Column
+    private String url;
 
     public Long getId()
     {
@@ -48,5 +63,15 @@ public class Script
     public void setUuid(String uuid)
     {
         this.uuid = uuid;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+
+    public void setUrl(String url)
+    {
+        this.url = url;
     }
 }
