4e369d785fbfd833b9676b41eca8d4d32b6ce8be
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-06-19 01:23:22 +0100

    fixed missing mapping to DTO on CommandController

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/CommandController.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/Controllers/CommandController.java
@@ -1,23 +1,32 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Controllers;
 
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.CommandDTO;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs.GermlineDTO;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Command;
+import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Germline;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Models.Tool;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.CommandService;
 import com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.Services.DatasetService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/command")
 @CrossOrigin(origins = "${app.resourceAllowedOrigins}")
 public class CommandController
 {
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Autowired
     private CommandService commandService;
 
@@ -32,8 +41,11 @@ public class CommandController
      */
     @GetMapping
     @Operation(summary = "Returns all Command objects stored")
-    public List<Command> getCommands()
+    public List<CommandDTO> getCommands()
     {
-        return commandService.readAll();
+        List<Command> commands = commandService.readAll();
+        List<CommandDTO> commandDTOs = commands.stream().map(command -> modelMapper.map(command, CommandDTO.class)).collect(Collectors.toList());
+
+        return commandDTOs;
     }
 }
