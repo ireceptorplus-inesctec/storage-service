dadd39801ea0b1bb915088ef61f215be679bbf19
Dinis <dinis.f.trigo@inesctec.pt>
2022-10-26 00:47:42 +0100

    fixed migration script

--- src/main/resources/db/migration/postgresql/V0_0_010_PopulateToolsAndCommands.sql
+++ src/main/resources/db/migration/postgresql/V0_0_010_PopulateToolsAndCommands.sql
@@ -14,6 +14,24 @@ values
         '4.0.0'
     );
 
+insert into Tool (
+    description,
+    docs_reference,
+    name,
+    url,
+    version
+)
+values
+    (
+        '',
+        'https://ncbi.github.io/igblast/',
+        'Igblast',
+        'https://ftp.ncbi.nih.gov/blast/executables/igblast/release/1.20.0/ncbi-igblast-1.20.0-src.zip',
+        '1.20.0'
+    );
+
+
+
 insert into Command (
     command_string,
     tool_id
@@ -23,3 +41,16 @@ values
         'align',
         '1'
     );
+
+
+insert into Command (
+    command_string,
+    tool_id
+)
+values
+    (
+        '-germline_db_V database/mouse_gl_V -germline_db_J database/mouse_gl_J -germline_db_D database/mouse_gl_D -organism mouse -query myseq -auxiliary_data optional_file/mouse_gl.aux -show_translation -outfmt 3',
+        '2'
+    );
+
+
