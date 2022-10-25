273fcb89c6f4711ff167b7e8cd26132a04e4de8e
Dinis <dinis.f.trigo@inesctec.pt>
2022-10-25 23:34:54 +0100

    fixed db migration

--- src/main/resources/db/migration/postgresql/V0_0_010_PopulateToolsAndCommands.sql
+++ src/main/resources/db/migration/postgresql/V0_0_010_PopulateToolsAndCommands.sql
@@ -6,20 +6,20 @@ insert into Tool (
     version
 )
 values
-(
- '',
- 'https://docs.milaboratories.com/',
- 'MiXCR',
- 'https://github.com/milaboratory/mixcr/releases/download/v4.0.0/mixcr-4.0.0.zip',
- '4.0.0'
-)
+    (
+        '',
+        'https://docs.milaboratories.com/',
+        'MiXCR',
+        'https://github.com/milaboratory/mixcr/releases/download/v4.0.0/mixcr-4.0.0.zip',
+        '4.0.0'
+    );
 
 insert into Command (
     command_string,
     tool_id
 )
 values
-(
-    'align',
-    '1'
-)
+    (
+        'align',
+        '1'
+    );
