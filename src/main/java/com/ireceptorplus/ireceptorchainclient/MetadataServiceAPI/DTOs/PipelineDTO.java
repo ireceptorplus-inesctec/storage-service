14b1760c0c8226a9e3b51fffcb6193fc409cc06d
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2022-11-28 00:13:44 +0000

    created DTO class to represent pipline (after executed)

--- src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/DTOs/PipelineDTO.java
+++ src/main/java/com/ireceptorplus/ireceptorchainclient/MetadataServiceAPI/DTOs/PipelineDTO.java
@@ -1,5 +1,24 @@
 package com.ireceptorplus.ireceptorchainclient.MetadataServiceAPI.DTOs;
 
-public class PipelineDTO
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public class PipelineDTO extends CreatedPipelineDTO
 {
+    @NotNull
+    private List<String> outputDatasetsUuids;
+
+    public PipelineDTO()
+    {
+    }
+
+    public List<String> getOutputDatasetsUuids()
+    {
+        return outputDatasetsUuids;
+    }
+
+    public void setOutputDatasetsUuids(List<String> outputDatasetsUuids)
+    {
+        this.outputDatasetsUuids = outputDatasetsUuids;
+    }
 }
