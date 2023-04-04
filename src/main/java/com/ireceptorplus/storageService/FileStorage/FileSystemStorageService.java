d162823a51d35b8d7e096d9f48f5e13234c5d997
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2023-04-05 00:34:05 +0100

    removed debugging prints of properties on FileSystemStorageService

--- src/main/java/com/ireceptorplus/storageService/FileStorage/FileSystemStorageService.java
+++ src/main/java/com/ireceptorplus/storageService/FileStorage/FileSystemStorageService.java
@@ -12,7 +12,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.stream.Stream;
 
 import com.ireceptorplus.storageService.Utils.FileUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.util.FileSystemUtils;
@@ -21,10 +20,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileSystemStorageService implements StorageService {
 
 	private final Path rootLocation;
-	@Value("${blockchain-api.hyperledger-fabric.blockchain.network-config-path}")
-	private String configPath;
-	@Value("${blockchain-api.hyperledger-fabric.blockchain.network-name}")
-	private String networkName;
 
 	public FileSystemStorageService(StorageProperties properties) {
 		this.rootLocation = Paths.get(properties.getLocation());
@@ -38,8 +33,6 @@ public class FileSystemStorageService implements StorageService {
 
 	@Override
 	public void store(MultipartFile file, String customName) {
-		System.out.println(configPath);
-		System.out.println(networkName);
 		try {
 			if (file.isEmpty()) {
 				throw new StorageException("Failed to store empty file.");
