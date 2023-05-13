b2061db1f1c45009c7a84ab9aa1ccce7107aa14c
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2023-05-13 02:40:53 +0100

    fixed JPQL query to get at most one pipeline in queue from DB

--- src/main/java/com/ireceptorplus/storageService/MetadataServiceAPI/Repositories/CreatedPipelineRepository.java
+++ src/main/java/com/ireceptorplus/storageService/MetadataServiceAPI/Repositories/CreatedPipelineRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CreatedPipelineRepository extends JpaRepository<CreatedPipeline, Long>
 {
-    @Query("SELECT * FROM CreatedPipeline WHERE state = 2 LIMIT 1")
+    @Query("SELECT pipeline FROM CreatedPipeline pipeline WHERE state = 2 LIMIT 1")
     public CreatedPipeline getOneInQueue();
 }
