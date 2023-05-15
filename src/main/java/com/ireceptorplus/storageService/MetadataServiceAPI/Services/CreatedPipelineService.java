59fddad8e98d9a2b148045e582b8f161a4b15766
Dinis Trigo <25772485+DinisTrigo@users.noreply.github.com>
2023-05-15 17:43:07 +0100

    changed mechanism of fetching CreatedPipeline in Queue to process from DB: used EntityManager to enable limiting the results to 1

--- src/main/java/com/ireceptorplus/storageService/MetadataServiceAPI/Services/CreatedPipelineService.java
+++ src/main/java/com/ireceptorplus/storageService/MetadataServiceAPI/Services/CreatedPipelineService.java
@@ -3,23 +3,31 @@ package com.ireceptorplus.storageService.MetadataServiceAPI.Services;
 import com.ireceptorplus.storageService.MetadataServiceAPI.Models.CreatedPipeline;
 import com.ireceptorplus.storageService.MetadataServiceAPI.Models.CreatedPipelineState;
 import com.ireceptorplus.storageService.MetadataServiceAPI.Repositories.CreatedPipelineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CreatedPipelineService extends CreateAndReadService<CreatedPipeline, Long>
 {
+    @Autowired
     CreatedPipelineRepository createdPipelineRepository;
 
 
-    protected CreatedPipelineService(CreatedPipelineRepository createdPipelineRepository)
+    @Autowired
+    private EntityManager entityManager;
+
+    public CreatedPipelineService(JpaRepository<CreatedPipeline, Long> repository, CreatedPipelineRepository createdPipelineRepository, EntityManager entityManager)
     {
-        super(createdPipelineRepository);
+        super(repository);
         this.createdPipelineRepository = createdPipelineRepository;
+        this.entityManager = entityManager;
     }
 
     public void delete(CreatedPipeline createdPipeline)
@@ -29,6 +37,6 @@ public class CreatedPipelineService extends CreateAndReadService<CreatedPipeline
 
     public CreatedPipeline getNextToProcess()
     {
-        return createdPipelineRepository.getOneInQueue();
+        return entityManager.createQuery("SELECT pipeline FROM CreatedPipeline pipeline WHERE pipeline.state = 2", CreatedPipeline.class).getSingleResult();
     }
 }
